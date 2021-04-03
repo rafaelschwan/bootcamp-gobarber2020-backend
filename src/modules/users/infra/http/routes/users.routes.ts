8092362e58e4bcec7876c02204b3b03a10dba519
Rafael Schwan <rafaelschwan@hotmail.com>
2021-04-03 12:40:08 -0300

    Termino da re-estruturação utilizando a metologia DDD

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,15 +1,17 @@
 import { Router } from 'express';
 import multer from 'multer';
 import uploadConfig from '@config/upload';
-import { container } from 'tsyringe';
 
 import UsersController from '../controllers/UsersController';
-import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
+import UserAvatarController from '../controllers/UserAvatarController';
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
+
 const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
+
 const upload = multer(uploadConfig);
 
 usersRouter.post('/', usersController.create);
@@ -18,26 +20,7 @@ usersRouter.patch(
   '/avatar',
   ensureAuthenticated,
   upload.single('avatar'),
-  async (request, response) => {
-
-    const UpdateUserAvatar = container.resolve(UpdateUserAvatarService);
-
-    const user = await UpdateUserAvatar.execute({
-      user_id: request.user.id,
-      avatarFilename: request.file.filename,
-    });
-
-    const userWithoutPassword = {
-      id: user.id,
-      name: user.name,
-      email: user.email,
-      avatar: user.avatar,
-      created_at: user.created_at,
-      updated_at: user.updated_at,
-    };
-
-    return response.json(userWithoutPassword);
-  }
+  userAvatarController.update
 );
 
 export default usersRouter;
